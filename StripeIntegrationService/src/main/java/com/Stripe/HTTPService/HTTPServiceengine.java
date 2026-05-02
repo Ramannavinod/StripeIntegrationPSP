d25d48f1d19b91056b8bab2a45c01feca74409e1
Vinodh <gowdavinod995@gmail.com>
2026-05-02 16:49:26 +0530

    Successfull created Checkout API

--- StripeIntegrationService/src/main/java/com/Stripe/HTTPService/HTTPServiceengine.java
+++ StripeIntegrationService/src/main/java/com/Stripe/HTTPService/HTTPServiceengine.java
@@ -1,13 +1,11 @@
-package com.Stripe.HTTPService;import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+package com.Stripe.HTTPService;import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClient;
 
-import com.Stripe.pojo.CreatePayment;
-import com.Stripe.pojo.Lineitems;
+import com.Stripe.exception.StripeExceptionHandling;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,36 +16,27 @@ public class HTTPServiceengine {
 	public HTTPServiceengine(RestClient.Builder builder) {
 		 this.restclient = builder.build();
   }
-	public ResponseEntity<String> createpaymenet(CreatePayment payment) {
-	HttpHeaders headers = new HttpHeaders();
-	headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-	headers.setBearerAuth("sk_test_tR3PYbcVNZZ796tH88S4VQ2u");
-	MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-
-    body.add("mode", payment.getMode());
-    body.add("success_url", payment.getSuccess_url());
-    body.add("cancel_url", payment.getCancel_url());
-
-    for (int i = 0; i < payment.getLine_items().size(); i++) {
-
-        Lineitems item = payment.getLine_items().get(i);
-
-        body.add("line_items[" + i + "][quantity]", String.valueOf(item.getQuantity()));
-        body.add("line_items[" + i + "][price_data][currency]", item.getCurrency());
-        body.add("line_items[" + i + "][price_data][unit_amount]", item.getUnitAmount());
-        body.add("line_items[" + i + "][price_data][product_data][name]", item.getProductName());
-    }
-
- 
-	 ResponseEntity<String> response=restclient.post()
-	.uri("https://api.stripe.com/v1/checkout/sessions")
-	.headers(h->h.addAll(headers))
-	.body(body)
+	public ResponseEntity<String> createpaymenet(HTTPReq httpreq) {
+	
+     try {
+	 ResponseEntity<String> response=restclient.method(httpreq.getMethod())
+	.uri(httpreq.getUrl())
+	.headers(h->h.addAll(httpreq.getHeaders()))
+	.body(httpreq.getBody())
 	.retrieve()
 	.toEntity(String.class);
 	 log.info("Stripe API response: {}", response.getBody());
 	
 	return response;
+     }
+     catch(HttpServerErrorException | HttpClientErrorException e) {
+        log.error("Stripe API error: {}", e.getResponseBodyAsString());
+        throw new StripeExceptionHandling("10002", "unable to connect with stripe", HttpStatus.BAD_GATEWAY.value());
+     }
+     catch(Exception e) {
+		 log.error("Error while calling Stripe API: {}", e.getMessage());
+		 throw new StripeExceptionHandling("10003", "An unexpected error occurred while processing the payment", HttpStatus.INTERNAL_SERVER_ERROR.value());
+	 }
 	}
 	
 
--- StripeIntegrationService/src/main/java/com/Stripe/Service/PaymentService.java
+++ StripeIntegrationService/src/main/java/com/Stripe/Service/PaymentService.java
@@ -1,10 +1,20 @@
 package com.Stripe.Service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import com.Stripe.HTTPService.HTTPReq;
 import com.Stripe.HTTPService.HTTPServiceengine;
+import com.Stripe.exception.StripeExceptionHandling;
 import com.Stripe.pojo.CreatePayment;
+import com.Stripe.pojo.Lineitems;
 import com.Stripe.serviceinter.PaymentServiceintern;
 
 import lombok.RequiredArgsConstructor;
@@ -16,11 +26,45 @@ import lombok.extern.slf4j.Slf4j;
 public class PaymentService implements PaymentServiceintern {
 	
 	private final HTTPServiceengine httpServiceengine;
+	
+	@Value("${stripe.secret.key}")
+	private String secret;
+	
+	@Value("${stripe.url}")
+    private String url;	
 
 	@Override
-	public ResponseEntity<String> CreatePaymentIntent(CreatePayment createPayment) {
+	public ResponseEntity<String> CreatePaymentIntent(CreatePayment payment) {
 		log.info("Creating Payment Intent");
-		ResponseEntity<String> response=httpServiceengine.createpaymenet(createPayment);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.setBearerAuth(secret);
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+	    body.add("mode", payment.getMode());
+	    body.add("success_url", payment.getSuccess_url());
+	    body.add("cancel_url", payment.getCancel_url());
+        if(payment.getCancel_url()==null) {
+        	throw new StripeExceptionHandling("10001","Cancel URL is required",HttpStatus.BAD_REQUEST.value());
+		}
+	    for (int i = 0; i < payment.getLine_items().size(); i++) {
+
+	        Lineitems item = payment.getLine_items().get(i);
+
+	        body.add("line_items[" + i + "][quantity]", String.valueOf(item.getQuantity()));
+	        body.add("line_items[" + i + "][price_data][currency]", item.getCurrency());
+	        body.add("line_items[" + i + "][price_data][unit_amount]", item.getUnitAmount());
+	        body.add("line_items[" + i + "][price_data][product_data][name]", item.getProductName());
+	    }
+        
+	    HTTPReq httpreq = HTTPReq.builder()
+		        .url(url)
+		        .method(HttpMethod.POST)
+		        .headers(headers)
+		        .body(body)
+		        .build();
+		ResponseEntity<String> response=httpServiceengine.createpaymenet(httpreq);
+		 log.info("Stripe API response: {}", response.getBody());
 		return response;
 	}
 	
